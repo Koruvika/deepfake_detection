afd661a3a3a4bc5303134cb19ac7f8d3f86d5205
koruvika <duongnpc239@gmail.com>
2023-11-01 19:25:24 +0700

    Update: Processed FFPP Dataset and Multi GPU UniMoCo Model

--- src/datasets/SupConDataset.py
+++ src/datasets/SupConDataset.py
@@ -348,9 +348,113 @@ class CelebSupConDataset(Dataset):
         return image, label
 
 
+
+class PreprocessedFaceForencisDataset(Dataset):
+    def __init__(self, configs, phase="train", transform=None, using_contrast=False):
+        super(Dataset, self).__init__()
+        self.configs = configs
+        self.phase = phase
+        self.using_contrast = using_contrast
+
+        if transform is None:
+            self.transform = default_transform
+        else:
+            self.transform = transform
+
+        if using_contrast:
+            self.contrast_transform = TwoCropTransform(transform)
+        else:
+            self.contrast_transform = transform
+
+        if self.phase == "train":
+            self.root_folder = self.configs.dataset.train_root
+        elif self.phase in ["test", "valid"]:
+            self.root_folder = self.configs.dataset.test_root
+        else:
+            raise "Phase is not valid"
+
+        images_folder = os.path.join(self.root_folder, "images")
+        landmarks_folder = os.path.join(self.root_folder, "landmarks")
+
+        list_folder = {  # folder and its label
+            "original": 1,
+            "DeepFakeDetection": 0,
+            "FaceSwap": 0,
+            "Face2Face": 0,
+            "FaceShifter": 0,
+            "NeuralTextures": 0,
+            "Deepfakes": 0,
+        }
+
+        self.images_file_list = []
+        self.landmarks_file_list = []
+        self.labels_list = []
+
+        for k, v in list_folder.items():
+            images_folder_path = os.path.join(images_folder, k)
+            landmarks_folder_path = os.path.join(landmarks_folder, k)
+
+            video_list = os.listdir(images_folder_path)
+            images_video_list = [os.path.join(images_folder_path, video) for video in video_list]
+            landmarks_video_list = [os.path.join(landmarks_folder_path, video) for video in video_list]
+
+            for images_video, landmarks_video in zip(images_video_list, landmarks_video_list):
+
+                image_samples_list = os.listdir(images_video)
+                image_samples_list.sort()
+                landmark_samples_list = os.listdir(landmarks_video)
+                landmark_samples_list.sort()
+
+                n = len(image_samples_list)
+                assert n == len(landmark_samples_list)
+
+                self.images_file_list = self.images_file_list + [os.path.join(images_video, fn) for fn in
+                                                                 image_samples_list]
+                self.landmarks_file_list = self.landmarks_file_list + [os.path.join(landmarks_video, fn) for fn in
+                                                                       landmark_samples_list]
+                self.labels_list += [v for _ in range(n)]
+
+    def __len__(self):
+        return len(self.images_file_list)
+
+    def __getitem__(self, item):
+        assert self.landmarks_file_list[item].split("/")[-1].split(".")[0] == \
+               self.images_file_list[item].split("/")[-1].split(".")[0]
+
+        # read image, landmarks and label
+        image = cv2.imread(self.images_file_list[item], cv2.IMREAD_UNCHANGED)
+        landmark = np.load(self.landmarks_file_list[item])
+        label = self.labels_list[item]
+
+        # get bounding
+        bbox_lm = np.array([landmark[:, 0].min(), landmark[:, 1].min(), landmark[:, 0].max(), landmark[:, 1].max()])
+        w = bbox_lm[2] - bbox_lm[0]
+        h = bbox_lm[3] - bbox_lm[1]
+        x0 = int(max(bbox_lm[0] - w * 0.1, 0))
+        y0 = int(max(bbox_lm[1] - h * 0.1, 0))
+        x1 = int(min(bbox_lm[2] + w * 0.1, image.shape[1]))
+        y1 = int(min(bbox_lm[3] + h * 0.1, image.shape[0]))
+        bbox = np.array([[x0, y0], [x1, y1]])
+
+        # re-order landmark
+        landmark = reorder_landmark(landmark)
+
+        # horizontal flip
+        if self.phase == 'train':
+            if np.random.rand() < 0.5:
+                image, _, landmark, bbox = hflip(image, None, landmark, bbox)
+
+        # Contrastive Transform
+        image = self.contrast_transform(Image.fromarray(image))
+
+        return image, label
+
+
 if __name__ == "__main__":
+    from yacs.config import CfgNode
     class A:
-        test_root = "/mnt/data/duongdhk/datasets/processed_deepfake_detection_dataset/Celeb-DF-v2/images"
+        dataset = CfgNode()
+        dataset.train_root = "/mnt/data/duongdhk/datasets/processed_deepfake_detection_dataset/processed_FFPP"
 
     train_transform = transforms.Compose([
         transforms.RandomResizedCrop(size=32, scale=(0.2, 1.)),
@@ -359,6 +463,6 @@ if __name__ == "__main__":
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    dataset = LinearDataset(A, "train", train_transform)
+    dataset = PreprocessedFaceForencisDataset(A, "train", train_transform, using_contrast=True)
     for data in dataset:
         print(data)
