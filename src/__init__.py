e006031e10d12d1a53a7f3e121791c6bba5e078e
koruvika <duongnpc239@gmail.com>
2023-11-03 20:18:22 +0700

    Temp: Remove unnecessary module 2

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from .optimizers import SAM, LinearDecayLR
-from .models import SBIDetector
-from src.datasets.SBIDataset import SBIFaceForencisDataset
-from .utils import RandomDownScale, random_get_hull, IoUfrom2bboxes, crop_face, dynamic_blend
-from .metrics import compute_accuracy
+# from .optimizers import SAM, LinearDecayLR
+# from .models import SBIDetector
+# from src.datasets.SBIDataset import SBIFaceForencisDataset
+# from .utils import RandomDownScale, random_get_hull, IoUfrom2bboxes, crop_face, dynamic_blend
+# from .metrics import compute_accuracy
